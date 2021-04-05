22914cfe916b52d9a4f42878ce89e5f83849c6f3
Konstantin <coostof@gmail.com>
2021-04-05 22:50:37 +0300

    AndroidAssertions and AndroidTest have been added

---
+++ app/src/test/java/com/konstantin/kustov/movie/AndroidAssertions.kt
@@ -0,0 +1,18 @@
+@file:JvmName("AndroidAssertions")
+@file:JvmMultifileClass
+
+package com.konstantin.kustov.movie
+
+import androidx.appcompat.app.AppCompatActivity
+import org.amshove.kluent.shouldEqual
+import org.robolectric.Robolectric
+import org.robolectric.Shadows
+import kotlin.reflect.KClass
+
+infix fun KClass<out AppCompatActivity>.shouldNavigateTo(nextActivity: KClass<out AppCompatActivity>): () -> Unit = {
+    val originActivity = Robolectric.buildActivity(this.java).get()
+    val shadowActivity = Shadows.shadowOf(originActivity)
+    val nextIntent = shadowActivity.peekNextStartedActivity()
+
+    nextIntent.component?.className shouldEqual nextActivity.java.canonicalName
+}
